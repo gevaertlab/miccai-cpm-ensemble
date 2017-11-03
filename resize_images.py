5e7449d7197f59c5a5ced3e42074acd8cea0ff99
romainsauvestre <sauvestre.romain@gmail.com>
2017-11-03 21:15:51 +0000

    script to resize rembrandt x3

--- resize_images.py
+++ resize_images.py
@@ -36,10 +36,10 @@ def resize_image(im_path, out_path, new_size):
 if __name__ == '__main__':
     new_size = (100,240,240)
 
-    in_path_train_dir = 'data/train/'
-    in_path_val_dir = 'data/val/'
-    out_path_train_dir = 'data/resized_{}/train/'.format(str(new_size))
-    out_path_val_dir = 'data/resized_{}/val/'.format(str(new_size))
+    in_path_train_dir = 'data/rembrandt/train/'
+    in_path_val_dir = 'data/rembrandt/val/'
+    out_path_train_dir = 'data/rembrandt/resized_{}/train/'.format(str(new_size))
+    out_path_val_dir = 'data/rembrandt/resized_{}/val/'.format(str(new_size))
 
     if not os.path.exists(out_path_train_dir):
         os.makedirs(out_path_train_dir)
@@ -54,9 +54,10 @@ if __name__ == '__main__':
         if not os.path.exists(out_im_dir_path):
             os.makedirs(out_im_dir_path)
         for im_name in os.listdir(in_im_dir_path):
-            im_path = os.path.join(in_im_dir_path, im_name)
-            out_path = os.path.join(out_im_dir_path, im_name)
-            resize_image(im_path, out_path, new_size)
+            if im_name[-3:] != 'npz':
+                im_path = os.path.join(in_im_dir_path, im_name)
+                out_path = os.path.join(out_im_dir_path, im_name)
+                resize_image(im_path, out_path, new_size)
 
     print('resizing images from validation set')
     for k, im_dir in enumerate(os.listdir(in_path_val_dir)):
@@ -66,8 +67,8 @@ if __name__ == '__main__':
         if not os.path.exists(out_im_dir_path):
             os.makedirs(out_im_dir_path)
         for im_name in os.listdir(in_im_dir_path):
-            im_path = os.path.join(in_im_dir_path, im_name)
-            out_path = os.path.join(out_im_dir_path, im_name)
-            resize_image(im_path, out_path, new_size)
+            if im_name[-3:] != 'npz':
+                im_path = os.path.join(in_im_dir_path, im_name)
+                out_path = os.path.join(out_im_dir_path, im_name)
+                resize_image(im_path, out_path, new_size)
 
-    
