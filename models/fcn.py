fix batch normalization

models/fcn.py
@@ -30,10 +30,9 @@ class FCN_Model(Model):
                                                 shape=[None, self.patch, self.patch, self.patch, 4])
         self.label_placeholder = tf.placeholder(tf.int32,
                                                 shape=[None, self.patch, self.patch, self.patch])
-        self.dropout_placeholder = tf.placeholder(tf.float32,
-                                                  shape=[])
-        self.lr_placeholder = tf.placeholder(tf.float32,
-                                             shape=[])
+        self.dropout_placeholder = tf.placeholder(tf.float32, shape=[])
+        self.lr_placeholder = tf.placeholder(tf.float32, shape=[])
+        self.is_training = tf.placeholder(tf.bool, shape = [])
 
     def add_model(self):
         batch_size = tf.shape(self.label_placeholder)[0]
@@ -174,7 +173,8 @@ class FCN_Model(Model):
             feed = {self.image_placeholder: x,
                     self.label_placeholder: y,
                     self.dropout_placeholder: self.config.dropout,
-                    self.lr_placeholder: lr}
+                    self.lr_placeholder: lr,
+                    self.is_training: True}
 
             pred, loss, _ = sess.run([self.pred, self.loss, self.train], feed_dict=feed)
 
@@ -197,7 +197,8 @@ class FCN_Model(Model):
             feed = {self.image_placeholder: x,
                     self.label_placeholder: y,
                     self.dropout_placeholder: self.config.dropout,
-                    self.lr_placeholder: lr}
+                    self.lr_placeholder: lr,
+                    self.is_training: True}
 
             pred, loss, _ = sess.run([self.pred, self.loss, self.train_last_layers], feed_dict=feed)
 
@@ -218,7 +219,8 @@ class FCN_Model(Model):
 
             feed = {self.image_placeholder: x,
                     self.label_placeholder: y,
-                    self.dropout_placeholder: 1.0}
+                    self.dropout_placeholder: 1.0,
+                    self.is_training: False}
 
             pred = sess.run(self.pred, feed_dict=feed)
             bdice = dice_score(y, pred)
@@ -237,7 +239,8 @@ class FCN_Model(Model):
 
             feed = {self.image_placeholder: x,
                     self.label_placeholder: y,
-                    self.dropout_placeholder: 1.0}
+                    self.dropout_placeholder: 1.0,
+                    self.is_training:False}
 
             pred, prob = sess.run([self.pred, self.prob], feed_dict=feed)
 

models/fcn_concat.py
@@ -19,7 +19,7 @@ class FCN_Concat(FCN_Model):
                                      kernel_initializer=tf.contrib.layers.xavier_initializer(),
                                      bias_initializer=tf.constant_initializer(0.0))
 
-            bn1 = tf.layers.batch_normalization(conv1, axis=-1)
+            bn1 = tf.layers.batch_normalization(conv1, axis=-1, training=self.is_training)
             relu1 = tf.nn.relu(bn1)
 
             # shape = (patch/2, patch/2, patch/2)
@@ -42,7 +42,7 @@ class FCN_Concat(FCN_Model):
                                      kernel_initializer=tf.contrib.layers.xavier_initializer(),
                                      bias_initializer=tf.constant_initializer(0.0))
 
-            bn2 = tf.layers.batch_normalization(conv2, axis=-1)
+            bn2 = tf.layers.batch_normalization(conv2, axis=-1, training=self.is_training)
             relu2 = tf.nn.relu(bn2)
 
             # shape = (patch/4, patch/4, patch/4)
@@ -63,7 +63,7 @@ class FCN_Concat(FCN_Model):
                                      kernel_initializer=tf.contrib.layers.xavier_initializer(),
                                      bias_initializer=tf.constant_initializer(0.0))
 
-            bn3 = tf.layers.batch_normalization(conv3, axis=-1)
+            bn3 = tf.layers.batch_normalization(conv3, axis=-1, training=self.is_training)
             relu3 = tf.nn.relu(bn3)
 
             # shape = (patch/8, patch/8, patch/8)
@@ -89,7 +89,7 @@ class FCN_Concat(FCN_Model):
                                    initializer=tf.zeros_initializer())
             deconv4 = deconv4 + bias
             # shape = (patch/4, patch/4, patch/4)
-            bn4 = tf.layers.batch_normalization(deconv4, axis=-1)
+            bn4 = tf.layers.batch_normalization(deconv4, axis=-1, training=self.is_training)
             bn4 = tf.concat([bn4, pool2], axis=-1)
             relu4 = tf.nn.relu(bn4)
 
@@ -109,7 +109,7 @@ class FCN_Concat(FCN_Model):
                                    initializer=tf.zeros_initializer())
             deconv5 = deconv5 + bias
             # shape = (patch/2, patch/2, patch/2)
-            bn5 = tf.layers.batch_normalization(deconv5, axis=-1)
+            bn5 = tf.layers.batch_normalization(deconv5, axis=-1, training=self.is_training)
             bn5 = tf.concat([bn5, pool1], axis=-1)
             relu5 = tf.nn.relu(bn5)
 
@@ -160,3 +160,9 @@ class FCN_Concat(FCN_Model):
                                      + tf.nn.l2_loss(w6))
 
         self.loss = ce_loss + reg_loss
+
+    def add_train_op(self):
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            self.train = tf.train.AdamOptimizer(learning_rate=self.lr_placeholder).minimize(self.loss)   
+