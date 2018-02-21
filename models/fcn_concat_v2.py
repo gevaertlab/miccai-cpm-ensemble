f29414bb5d4ba2498d516e823d81a647f0a85027
romainsauvestre <sauvestre.romain@gmail.com>
2018-02-20 18:19:57 -0800

    new fcn concat v2: fix number of filters + add conv at the end + add dropout at conv3

--- models/fcn_concat_v2.py
+++ models/fcn_concat_v2.py
@@ -9,7 +9,7 @@ class FCN_Concat_v2(FCN_Concat):
         k_size = self.config.kernel_size
 
         with tf.variable_scope('conv1'):
-            conv1 = tf.layers.conv3d(inputs=self.image,
+            conv1_1 = tf.layers.conv3d(inputs=self.image,
                                      filters=nb_filters,
                                      kernel_size=k_size,
                                      strides=(1, 1, 1),
@@ -19,10 +19,10 @@ class FCN_Concat_v2(FCN_Concat):
                                      kernel_initializer=tf.contrib.layers.xavier_initializer(),
                                      bias_initializer=tf.constant_initializer(0.0),
                                      kernel_regularizer=tf.nn.l2_loss)
-            bn1 = tf.layers.batch_normalization(conv1, axis=-1, training=self.is_training)
-            relu1 = tf.nn.relu(bn1)
+            bn1_1 = tf.layers.batch_normalization(conv1_1, axis=-1, training=self.is_training)
+            relu1_1 = tf.nn.relu(bn1_1)
 
-            conv1 = tf.layers.conv3d(inputs=relu1,
+            conv1_2 = tf.layers.conv3d(inputs=relu1_1,
                                      filters=nb_filters,
                                      kernel_size=k_size,
                                      strides=(1, 1, 1),
@@ -32,17 +32,17 @@ class FCN_Concat_v2(FCN_Concat):
                                      kernel_initializer=tf.contrib.layers.xavier_initializer(),
                                      bias_initializer=tf.constant_initializer(0.0),
                                      kernel_regularizer=tf.nn.l2_loss)
-            bn1 = tf.layers.batch_normalization(conv1, axis=-1, training=self.is_training)
-            relu1 = tf.nn.relu(bn1)
+            bn1_2 = tf.layers.batch_normalization(conv1_2, axis=-1, training=self.is_training)
+            relu1_2 = tf.nn.relu(bn1_2)
 
             # shape = (patch/2, patch/2, patch/2)
-            pool1 = tf.layers.max_pooling3d(inputs=relu1, pool_size=(2, 2, 2),
+            pool1_2 = tf.layers.max_pooling3d(inputs=relu1_2, pool_size=(2, 2, 2),
                                             strides=(2, 2, 2), padding='VALID')
 
-            # drop1 = tf.nn.dropout(pool1, self.dropout_placeholder)
+            # drop1 = tf.nn.dropout(pool1_2, self.dropout_placeholder)
 
         with tf.variable_scope('conv2'):
-            conv2 = tf.layers.conv3d(inputs=pool1,
+            conv2_1 = tf.layers.conv3d(inputs=pool1_2,
                                      filters=2 * nb_filters,
                                      kernel_size=k_size,
                                      strides=(1, 1, 1),
@@ -53,10 +53,10 @@ class FCN_Concat_v2(FCN_Concat):
                                      bias_initializer=tf.constant_initializer(0.0),
                                      kernel_regularizer=tf.nn.l2_loss)
 
-            bn2 = tf.layers.batch_normalization(conv2, axis=-1, training=self.is_training)
-            relu2 = tf.nn.relu(bn2)
+            bn2_1 = tf.layers.batch_normalization(conv2_1, axis=-1, training=self.is_training)
+            relu2_1 = tf.nn.relu(bn2_1)
 
-            conv2 = tf.layers.conv3d(inputs=relu2,
+            conv2_2 = tf.layers.conv3d(inputs=relu2_1,
                                      filters=2 * nb_filters,
                                      kernel_size=k_size,
                                      strides=(1, 1, 1),
@@ -67,16 +67,16 @@ class FCN_Concat_v2(FCN_Concat):
                                      bias_initializer=tf.constant_initializer(0.0),
                                      kernel_regularizer=tf.nn.l2_loss)
 
-            bn2 = tf.layers.batch_normalization(conv2, axis=-1, training=self.is_training)
-            relu2 = tf.nn.relu(bn2)
+            bn2_2 = tf.layers.batch_normalization(conv2_2, axis=-1, training=self.is_training)
+            relu2_2 = tf.nn.relu(bn2_2)
 
             # shape = (patch/4, patch/4, patch/4)
-            pool2 = tf.layers.max_pooling3d(inputs=relu2, pool_size=(2, 2, 2),
+            pool2_2 = tf.layers.max_pooling3d(inputs=relu2_2, pool_size=(2, 2, 2),
                                             strides=(2, 2, 2), padding='VALID')
             # drop2 = tf.nn.dropout(pool2, self.dropout_placeholder)
 
         with tf.variable_scope('conv3'):
-            conv3 = tf.layers.conv3d(inputs=pool2,
+            conv3_1 = tf.layers.conv3d(inputs=pool2_2,
                                      filters=4 * nb_filters,
                                      kernel_size=k_size,
                                      strides=(1, 1, 1),
@@ -87,10 +87,11 @@ class FCN_Concat_v2(FCN_Concat):
                                      bias_initializer=tf.constant_initializer(0.0),
                                      kernel_regularizer=tf.nn.l2_loss)
 
-            bn3 = tf.layers.batch_normalization(conv3, axis=-1, training=self.is_training)
-            relu3 = tf.nn.relu(bn3)
+            bn3_1 = tf.layers.batch_normalization(conv3_1, axis=-1, training=self.is_training)
+            relu3_1 = tf.nn.relu(bn3_1)
+            drop3_1 = tf.nn.dropout(relu3_1, self.dropout_placeholder)
 
-            conv3 = tf.layers.conv3d(inputs=relu3,
+            conv3_2 = tf.layers.conv3d(inputs=drop3_1,
                                      filters=4 * nb_filters,
                                      kernel_size=k_size,
                                      strides=(1, 1, 1),
@@ -101,18 +102,18 @@ class FCN_Concat_v2(FCN_Concat):
                                      bias_initializer=tf.constant_initializer(0.0),
                                      kernel_regularizer=tf.nn.l2_loss)
 
-            bn3 = tf.layers.batch_normalization(conv3, axis=-1, training=self.is_training)
-            relu3 = tf.nn.relu(bn3)
+            bn3_2 = tf.layers.batch_normalization(conv3_2, axis=-1, training=self.is_training)
+            relu3_2 = tf.nn.relu(bn3_2)
 
             # shape = (patch/8, patch/8, patch/8)
-            pool3 = tf.layers.max_pooling3d(inputs=relu3, pool_size=(2, 2, 2),
+            pool3_2 = tf.layers.max_pooling3d(inputs=relu3_2, pool_size=(2, 2, 2),
                                             strides=(2, 2, 2), padding='VALID')
-            drop3 = tf.nn.dropout(pool3, self.dropout_placeholder)
+            drop3_2 = tf.nn.dropout(pool3_2, self.dropout_placeholder)
 
             # print(conv.get_shape())
 
         with tf.variable_scope('deconv4'):
-            deconv4 = tf.layers.conv3d_transpose(inputs=drop3,
+            deconv4_1 = tf.layers.conv3d_transpose(inputs=drop3_2,
                                                  filters=2 * nb_filters,
                                                  kernel_size=k_size,
                                                  strides=(2, 2, 2),
@@ -123,17 +124,17 @@ class FCN_Concat_v2(FCN_Concat):
                                                  bias_initializer=tf.zeros_initializer(),
                                                  kernel_regularizer=tf.nn.l2_loss)
 
-            bias = tf.get_variable('biases', [2 * nb_filters],
+            bias_4 = tf.get_variable('biases', [2 * nb_filters],
                                    initializer=tf.zeros_initializer())
-            deconv4 = deconv4 + bias
+            deconv4_1 = deconv4_1 + bias_4
             # shape = (patch/4, patch/4, patch/4)
-            bn4 = tf.layers.batch_normalization(deconv4, axis=-1, training=self.is_training)
-            bn4 = tf.concat([bn4, pool2], axis=-1)
-            relu4 = tf.nn.relu(bn4)
-            drop4 = tf.nn.dropout(relu4, self.dropout_placeholder)
+            bn4_1 = tf.layers.batch_normalization(deconv4_1, axis=-1, training=self.is_training)
+            bn4_1 = tf.concat([bn4_1, pool2], axis=-1)
+            relu4_1 = tf.nn.relu(bn4_1)
+            drop4_1 = tf.nn.dropout(relu4_1, self.dropout_placeholder)
 
-            conv4 = tf.layers.conv3d(inputs=drop4,
-                                     filters=4 * nb_filters,
+            conv4_2 = tf.layers.conv3d(inputs=drop4_1,
+                                     filters=2 * nb_filters,
                                      kernel_size=k_size,
                                      strides=(1, 1, 1),
                                      padding='SAME',
@@ -143,12 +144,12 @@ class FCN_Concat_v2(FCN_Concat):
                                      bias_initializer=tf.constant_initializer(0.0),
                                      kernel_regularizer=tf.nn.l2_loss)
 
-            bn4 = tf.layers.batch_normalization(conv4, axis=-1, training=self.is_training)
-            relu4 = tf.nn.relu(bn4)
-            drop4 = tf.nn.dropout(relu4, self.dropout_placeholder)
+            bn4_2 = tf.layers.batch_normalization(conv4_2, axis=-1, training=self.is_training)
+            relu4_2 = tf.nn.relu(bn4_2)
+            drop4_2 = tf.nn.dropout(relu4_2, self.dropout_placeholder)
 
         with tf.variable_scope('deconv5'):
-            deconv5 = tf.layers.conv3d_transpose(inputs=drop4,
+            deconv5_1 = tf.layers.conv3d_transpose(inputs=drop4_2,
                                                  filters=nb_filters,
                                                  kernel_size=k_size,
                                                  strides=(2, 2, 2),
@@ -158,18 +159,18 @@ class FCN_Concat_v2(FCN_Concat):
                                                  kernel_initializer=tf.contrib.layers.xavier_initializer(),
                                                  bias_initializer=tf.constant_initializer(0.0),
                                                  kernel_regularizer=tf.nn.l2_loss)
-            bias = tf.get_variable('biases', [nb_filters],
+            bias_5 = tf.get_variable('biases', [nb_filters],
                                    initializer=tf.zeros_initializer())
-            deconv5 = deconv5 + bias
+            deconv5_1 = deconv5_1 + bias_5
             # shape = (patch/2, patch/2, patch/2)
-            bn5 = tf.layers.batch_normalization(deconv5, axis=-1, training=self.is_training)
-            bn5 = tf.concat([bn5, pool1], axis=-1)
-            relu5 = tf.nn.relu(bn5)
+            bn5_1 = tf.layers.batch_normalization(deconv5_1, axis=-1, training=self.is_training)
+            bn5_1 = tf.concat([bn5_1, pool1], axis=-1)
+            relu5_1 = tf.nn.relu(bn5_1)
 
-            drop5 = tf.nn.dropout(relu5, self.dropout_placeholder)
+            drop5_1 = tf.nn.dropout(relu5_1, self.dropout_placeholder)
 
-            conv5 = tf.layers.conv3d(inputs=drop5,
-                                     filters=4 * nb_filters,
+            conv5_2 = tf.layers.conv3d(inputs=drop5_1,
+                                     filters=nb_filters,
                                      kernel_size=k_size,
                                      strides=(1, 1, 1),
                                      padding='SAME',
@@ -179,15 +180,15 @@ class FCN_Concat_v2(FCN_Concat):
                                      bias_initializer=tf.constant_initializer(0.0),
                                      kernel_regularizer=tf.nn.l2_loss)
 
-            bn5 = tf.layers.batch_normalization(conv5, axis=-1, training=self.is_training)
-            relu5 = tf.nn.relu(bn5)
-            drop5 = tf.nn.dropout(relu5, self.dropout_placeholder)
+            bn5_2 = tf.layers.batch_normalization(conv5_2, axis=-1, training=self.is_training)
+            relu5_2 = tf.nn.relu(bn5_2)
+            drop5_2 = tf.nn.dropout(relu5_2, self.dropout_placeholder)
 
             # print(deconv.get_shape())
             # print(relu5.get_shape())
 
         with tf.variable_scope('deconv6'):
-            deconv6 = tf.layers.conv3d_transpose(inputs=drop5,
+            deconv6_1 = tf.layers.conv3d_transpose(inputs=drop5_2,
                                                  filters=self.nb_classes,
                                                  kernel_size=k_size,
                                                  strides=(2, 2, 2),
@@ -199,6 +200,22 @@ class FCN_Concat_v2(FCN_Concat):
                                                  kernel_regularizer=tf.nn.l2_loss)
 
             # print(deconv6.get_shape())
-            bias = tf.get_variable('biases', [self.nb_classes],
+            bias_6 = tf.get_variable('biases', [self.nb_classes],
                                    initializer=tf.zeros_initializer())
-            self.score = deconv6 + bias
+
+            bn6_1 = tf.layers.batch_normalization(deconv5_1, axis=-1, training=self.is_training)
+            relu6_1 = tf.nn.relu(bn6_1)
+            drop6_1 = tf.nn.dropout(relu6_1, self.dropout_placeholder)
+
+            conv6_2 = tf.layers.conv3d(inputs=drop6_1,
+                                     filters=4 * nb_filters,
+                                     kernel_size=k_size,
+                                     strides=(1, 1, 1),
+                                     padding='SAME',
+                                     activation=None,
+                                     use_bias=True,
+                                     kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                     bias_initializer=tf.constant_initializer(0.0),
+                                     kernel_regularizer=tf.nn.l2_loss)
+
+            self.score = conv6_2
