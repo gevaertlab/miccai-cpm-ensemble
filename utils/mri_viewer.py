b4d5a71563c18e36d56b344b6c5169c236458bf1
Marc Thibault <marcthib@stanford.edu>
2018-05-29 11:10:19 -0700

    Update preprocessing code.

--- utils/mri_viewer.py
+++ utils/mri_viewer.py
@@ -93,3 +93,52 @@ class MRIViewer:
         im4 = self.im4[self.i, :, :]
         self.ax4.imshow(im4, vmin=self.vmin4, vmax=self.vmax4,
                         cmap=self.cmap, interpolation=None)
+
+        
+class MRIViewerOne:
+
+    def __init__(self, im1, cmap=None, title1='Image 1'):
+        self.i = 0
+
+        self.im1 = im1
+        self.vmin1 = self.im1.min()
+        self.vmax1 = self.im1.max()
+        self.title1 = title1
+
+        [self.depth, _, _] = self.im1.shape
+
+        self.cmap = cmap
+
+        self.fig = plt.figure()
+        
+        gs = gridspec.GridSpec(3, 1, height_ratios=[10, 1, 1])
+
+        self.ax1 = plt.subplot(gs[0, 0])
+        self.ax1.grid(False)
+        self.ax1.set_xticklabels([])
+        self.ax1.set_yticklabels([])
+        self.ax1.set_title(title1)
+
+
+        self.slax = plt.subplot(gs[2, :])
+        self.slider = Slider(self.slax, 'Slice',
+                      0.01, self.depth - 0.01, valinit=0, valfmt='%d')
+
+        def update(val):
+            self.i = int(val)
+            self.draw()
+            self.fig.canvas.draw()
+
+        self.slider.on_changed(update)
+
+        self.draw()
+        self.fig.canvas.draw()
+
+        plt.ion()
+        plt.show()
+
+    def draw(self):
+        im1 = self.im1[self.i, :, :]
+        self.ax1.imshow(im1, vmin=self.vmin1, vmax=self.vmax1,
+                        cmap=self.cmap, interpolation=None)
+        
--- utils/process_rtog_nii.py
+++ utils/process_rtog_nii.py
@@ -5,9 +5,9 @@ A file containing methods to process RTOG .nii files for use in deep learning.
 import os
 import shutil
 import subprocess
-import tempfile
 
-def _preproc(nii, modality, nii_out):
+
+def _preproc(nii, modality, nii_out, working_dir):
     """
     Run fsl_anat to preprocess a brain.
 
@@ -16,19 +16,27 @@ def _preproc(nii, modality, nii_out):
     modality: modality of the brain ('T1' or 'T2')
     nii_out: path to output processed .nii file to
     """
-    tmp_dir = os.path.abspath('tmp')
-    os.mkdir(tmp_dir)
-    subprocess.call(['fsl_anat',
+    tmp_dir = os.path.join(working_dir, 'tmp')
+    try:
+        os.mkdir(tmp_dir)
+    except FileExistsError:
+        pass
+
+    command_list = ['fsl_anat',
                      '-i', nii,
                      '-o', os.path.join(tmp_dir, modality),
                      '-t', modality,
                      '--noreg', '--nononlinreg',
-                     '--noseg', '--nosubcortseg'])
-    os.rename(os.path.join(tmp_dir, 
-              '{}.anat'.format(modality),
-              '{}_biascorr.nii.gz'.format(modality)),
+                     '--noseg', '--nosubcortseg']
+    print(" ".join(command_list))
+    subprocess.call(command_list)
+
+    os.rename(os.path.join(tmp_dir,
+                           '{}.anat'.format(modality),
+                           '{}_biascorr.nii.gz'.format(modality)),
               nii_out + '.gz')
     shutil.rmtree(tmp_dir)
+
 
 def _skull_strip(nii, nii_out, f=0.5, g=0.):
     """
@@ -40,7 +48,14 @@ def _skull_strip(nii, nii_out, f=0.5, g=0.):
     f: the f param for bet (default: 0.5)
     g: the g param for bet (default: 0.)
     """
-    subprocess.call(['bet', nii, nii_out, '-f', str(f), '-g', str(g)])
+    command_list = ['bet',
+                    nii,
+                    nii_out,
+                    '-f', str(f),
+                    '-g', str(g)]
+    print(" ".join(command_list))
+    subprocess.call(command_list)
+
 
 def _register(nii, ref, nii_out):
     """
@@ -52,17 +67,23 @@ def _register(nii, ref, nii_out):
     nii_out: path to output processed .nii file to
     """
     aff_file = os.path.abspath('reg.txt')
-    subprocess.call(['reg_aladin',
+    command_list = ['reg_aladin',
                      '-ref', ref + '.gz',
                      '-flo', nii + '.gz',
                      '-aff', aff_file,
                      '-res', nii_out,
-                     '-rigOnly'])
+                     '-rigOnly']
+    print(" ".join(command_list))
+    subprocess.call(command_list)
     os.remove(aff_file)
+
 
 ################################################################################
 
-def process_rtog_nii(nii_dir, f=0.5, g=0.):
+def process_rtog_nii(nii_dir,
+                     input_t1c_filename='t1c.nii', output_t1c_filename='t1c_proc.nii',
+                     input_flair_filename='flair.nii', output_flair_filename='flair_proc.nii',
+                     f=0.5, g=0.):
     """
     Process a pair of t1c and flair RTOG .nii files.
 
@@ -74,22 +95,30 @@ def process_rtog_nii(nii_dir, f=0.5, g=0.):
     This method creates two files t1c_proc.nii and flair_proc.nii in nii_dir.
     """
     nii_dir = os.path.abspath(nii_dir)
-    _preproc(os.path.join(nii_dir, 't1c.nii'), 'T1', 
-             os.path.join(nii_dir, 't1c_fsl_anat.nii'))
-    _preproc(os.path.join(nii_dir, 'flair.nii'), 'T2', 
-             os.path.join(nii_dir, 'flair_fsl_anat.nii'))
+    _preproc(os.path.join(nii_dir, input_t1c_filename),
+             'T1',
+             os.path.join(nii_dir, 't1c_fsl_anat.nii'),
+             nii_dir)
+    _preproc(os.path.join(nii_dir, input_flair_filename),
+             'T2',
+             os.path.join(nii_dir, 'flair_fsl_anat.nii'),
+             nii_dir)
+
     _skull_strip(os.path.join(nii_dir, 't1c_fsl_anat.nii'),
                  os.path.join(nii_dir, 't1c_bet.nii'), f=f, g=g)
     _skull_strip(os.path.join(nii_dir, 'flair_fsl_anat.nii'),
                  os.path.join(nii_dir, 'flair_bet.nii'), f=f, g=g)
+
     _register(os.path.join(nii_dir, 'flair_bet.nii'),
               os.path.join(nii_dir, 't1c_bet.nii'),
               os.path.join(nii_dir, 'flair_reg_aladin.nii'))
+
     subprocess.call(['gunzip', os.path.join(nii_dir, 't1c_bet.nii.gz')])
     os.rename(os.path.join(nii_dir, 't1c_bet.nii'),
-              os.path.join(nii_dir, 't1c_proc.nii'))
+              os.path.join(nii_dir, output_t1c_filename))
     os.rename(os.path.join(nii_dir, 'flair_reg_aladin.nii'),
-              os.path.join(nii_dir, 'flair_proc.nii'))
+              os.path.join(nii_dir, output_flair_filename))
+
     os.remove(os.path.join(nii_dir, 't1c_fsl_anat.nii.gz'))
     os.remove(os.path.join(nii_dir, 'flair_fsl_anat.nii.gz'))
     os.remove(os.path.join(nii_dir, 'flair_bet.nii.gz'))
